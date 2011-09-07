775300423e0e7cdac94dee02afd0a31bba768810
Hans-Peter Gygax <gygax@practicomp.ch>
2011-09-07 20:33:43 +0200

    Fixed quite a few bugs in WebServer module, however IMAP still not working; Hixie still needs to be re-implemented

--- webserver/webserver.c
+++ webserver/webserver.c
@@ -47,6 +47,15 @@ struct _wsv_url_parsing_struct {
 #define usleep Sleep
 #define strcasecmp stricmp
 #define usleep Sleep
+#define snprintf sprintf_s
+#endif
+
+/* Constants */
+
+#ifdef _WIN32
+static const int SOCKET_WOULDBLOCK = WSAEWOULDBLOCK;
+#else
+static const int SOCKET_WOULDBLOCK = EWOULDBLOCK;
 #endif
 
 /* Global variables */
@@ -56,17 +65,79 @@ static int ssl_initialized = 0;
 
 /* Logging/Tracing */
 
-#define __LOG(stream, ...) \
+#define __LOG(stream, format, ...) \
 if (! daemonized) { \
-    fprintf(stream, __VA_ARGS__); \
-    fprintf(stream, "\n" ); \
-    }
+	char buf[4096+1]; \
+	int offs = 0; \
+	offs += snprintf(buf+offs, 4096, format, __VA_ARGS__); \
+	offs += snprintf(buf+offs, 4096-offs, "\n"); \
+	fputs(buf, stream); \
+	fflush(stream); \
+}
     
-#define LOG_MSG(...) __LOG(stdout, __VA_ARGS__);
-#define LOG_ERR(...) __LOG(stderr, __VA_ARGS__);
+#define LOG_MSG(format, ...) __LOG(stdout, format, __VA_ARGS__);
+#define LOG_ERR(format, ...) __LOG(stderr, format, __VA_ARGS__);
 #define LOG_DBG LOG_MSG
 
 /* Private routines */
+
+#ifdef _WIN32
+
+static int
+get_socket_error()
+{
+	return WSAGetLastError();
+}
+
+static const char *
+describe_socket_error(int errCode)
+{
+	LPSTR errString;
+	int size;
+
+	// Get the error code
+	if (errCode == 0)
+		errCode = WSAGetLastError();
+
+    // ..and the human readable error string!!
+    // Interesting:  Also retrievable by net helpmsg 10060
+    errString = NULL;  // will be allocated and filled by FormatMessage
+
+    size = FormatMessageA( FORMAT_MESSAGE_ALLOCATE_BUFFER |
+        FORMAT_MESSAGE_FROM_SYSTEM,		// use windows internal message table
+        0,								// 0 since source is internal message table
+        errCode,						// this is the error code returned by WSAGetLastError()
+										// Could just as well have been an error code from generic
+										// Windows errors from GetLastError()
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_SYS_DEFAULT),	// preferring English
+        (LPSTR)&errString,				// this is WHERE we want FormatMessage
+										// to plunk the error string.  Note the
+										// peculiar pass format:  Even though
+										// errString is already a pointer, we
+										// pass &errString (which is really type LPSTR* now)
+										// and then CAST IT to (LPSTR).  This is a really weird
+										// trip up.. but its how they do it on msdn:
+										// http://msdn.microsoft.com/en-us/library/ms679351(VS.85).aspx
+        0,								// min size for buffer
+        0 );							// 0, since getting message from system tables
+     //printf( "Error code %d:  %s\n\nMessage was %d bytes, in case you cared to know this.\n\n", errCode, errString, size ) ;
+
+     //LocalFree( errString );
+
+	return errString;
+}
+
+#else
+
+static int
+get_socket_error()
+{
+	return errno();
+}
+
+// TODO: **nx implementation
+
+#endif // _WIN32
 
 static void 
 context_free(wsv_ctx_t *ctx) 
@@ -131,13 +202,13 @@ create_context_ssl(int socket, wsv_settings_t *settings)
 
     if (SSL_CTX_use_PrivateKey_file(ctx->ssl_ctx, use_keyfile,
                                     SSL_FILETYPE_PEM) <= 0) {
-        LOG_ERR("Unable to load private key file %s\n", use_keyfile);
+        LOG_ERR("Unable to load private key file %s", use_keyfile);
         goto fail;
     }
 
     if (SSL_CTX_use_certificate_file(ctx->ssl_ctx, settings->certfile,
                                      SSL_FILETYPE_PEM) <= 0) {
-        LOG_ERR("Unable to load certificate file %s\n", settings->certfile);
+        LOG_ERR("Unable to load certificate file %s", settings->certfile);
         goto fail;
     }
 
@@ -219,13 +290,11 @@ handle_request(int conn_id, int sockfd, wsv_settings_t *settings)
 		len = recv(sockfd, header, sizeof(header)-1, 0);
 		if (len > 0)
 			break;
-		else if (len == SOCKET_ERROR) {
-			LOG_ERR("Socket error while trying to receive HTTP header");
-			return;
-		}
 		else if (len < 0) {
-			LOG_ERR("Error receiving header - return code is %d", len);
-			return;
+			err = get_socket_error();
+			if (err != SOCKET_WOULDBLOCK) {
+				LOG_ERR("Socket error while trying to receive HTTP header: %s", describe_socket_error(err));
+				return; }
 		}
 		usleep(100);
 	}
@@ -287,7 +356,7 @@ handle_request(int conn_id, int sockfd, wsv_settings_t *settings)
     // We're done
     shutdown(sockfd, SHUT_RDWR);
     close(sockfd);
-    LOG_DBG("Request handled!\n-------------------------------------------");
+    LOG_DBG("Request %d handled!\n-------------------------------------------", conn_id);
     
     return;
     
@@ -556,7 +625,7 @@ wsv_serve_file(wsv_ctx_t *ctx, const char *path, const char *content_type)
         if (nr <= 0)
             break;
         ns = wsv_sendall(ctx, buf, nr);
-        LOG_DBG("Served %d bytes of file \"%s\"", ns, path); 
+        //LOG_DBG("Served %d bytes of file \"%s\"", ns, path); 
     }
 
     close(fd);
@@ -917,30 +986,31 @@ wsv_send(wsv_ctx_t *ctx, const void *pbuf, size_t blen)
     } else {
         size = send(ctx->sockfd, (char*) pbuf, blen, 0);
         if (size < 0) {
-			#ifdef _WIN32
-            err = WSAGetLastError();
-			#else
-            err = errno;
-			#endif
-            LOG_ERR("%s: send() failed (return value = %d), error is: %d", __FUNCTION__, size, err);
-            return size;
-        }
-        return size;
+			if (size != WSVSR_WAIT)
+				LOG_ERR("%s: send() failed (return value = %d), error is: %s", __FUNCTION__, size, describe_socket_error(0));
+		}
+		return size;
     }
 }
 
+// TODO: use select() ?
 ssize_t
 wsv_sendall(wsv_ctx_t *ctx, const void *pbuf, size_t blen)
 {
-    size_t total, slen;
+    size_t total;
+	int slen;
     total = 0;
     while (1) {
         slen = wsv_send(ctx, (char*) pbuf + total, blen - total);
-        if (slen < 0) return slen;
-        total += slen;
-        if (total >= blen) 
-            return (ssize_t) total;
-        usleep(1);
+        if (slen < 0) {
+			if (slen != WSVSR_WAIT) return slen;
+		}
+		else {
+			total += (size_t) slen;
+			if (total >= blen) 
+				return (ssize_t) total;
+		}
+        usleep(10);
     }
     LOG_DBG("%s failed", __FUNCTION__);
     return -1;
@@ -950,11 +1020,8 @@ ssize_t
 wsv_recv(wsv_ctx_t *ctx, void *pbuf, size_t blen)
 {
     int size;
-	int wouldblock;
     int err;
 
-    //LOG_DBG("%s: blen = %u", __FUNCTION__, blen);
-    
     if (ctx->ssl) {
         LOG_DBG("SSL recv");
         size = SSL_read(ctx->ssl, pbuf, blen);
@@ -967,24 +1034,23 @@ wsv_recv(wsv_ctx_t *ctx, void *pbuf, size_t blen)
             case SSL_ERROR_WANT_X509_LOOKUP:
                 return WSVSR_WAIT;
             default:
+				LOG_DBG("%s: error receiving from SSL, SSL code = %d", __FUNCTION__, size, err);
                 return WSVSR_CONNECTION_LOST;
             }
         }
         return size;
     } else {
-        //LOG_DBG("TCP recv");
         size = recv(ctx->sockfd, (char*) pbuf, blen, 0);
         if (size < 0) {
-			#ifdef _WIN32
-			err = WSAGetLastError();
-			wouldblock = err == WSAEWOULDBLOCK;
-			#else
-			err = errno();
-			wouldblock = err == EWOULDBLOCK;
-			#endif
-            if (!wouldblock)
-				LOG_DBG("%s: error receiving from socket, code = %d, errno = %d", __FUNCTION__, size, errno);
-			return wouldblock ? WSVSR_WAIT : WSVSR_CONNECTION_LOST;
+			err = get_socket_error();
+            if (err == SOCKET_WOULDBLOCK) {
+				return WSVSR_WAIT;
+			}
+			else {
+				LOG_DBG("%s: error receiving from socket, code = %d, errno = %d (%s)", 
+					__FUNCTION__, size, err, describe_socket_error(err));
+				return WSVSR_CONNECTION_LOST;
+			}
 		}
         return size;
     }
